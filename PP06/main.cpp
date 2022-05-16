7e615e4e21017dfcf9e45e9d426872d103883f16
YUHAN <YUHAN@PC7207_32>
2022-05-16 09:45:33 +0900

    초기 커밋.

---
+++ PP06/main.cpp
@@ -0,0 +1,29 @@
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+template<typename T>
+T sum(T a, T b)
+{
+	return a + b;
+}
+
+
+int main()
+{
+	vector<int> a;
+
+	cout << sum<double>(3.9, 4.9) << endl;
+
+	cout << a.size() << endl;
+
+	a.push_back(1);
+	a.push_back(2);
+	a.push_back(4);
+
+	cout << a.size() << endl;
+	cout << "б╞иг :" << a[0] << endl;
+
+	return 0;
+}
